329b859a2c0a5e9f8f55b053b1cbd56a2f2f2d57
Nathan Schulzke <nschulzke@gmail.com>
2018-03-13 17:14:29 -0600

    Gett the client working (basically)

--- model/Mancala.js
+++ model/Mancala.js
@@ -6,45 +6,48 @@ const P1 = 0;
 const P2 = 1;
 
 module.exports = class Mancala {
-    constructor() {
-        this.holes = [];
-        for (let i = 0; i < HOLES; i++) {
-            this.holes[i] = START_STONES;
-        }
-        this.holes[P1_DEST] = 0;
-        this.holes[P2_DEST] = 0;
-        this.turn = P1;
+  constructor() {
+    this.holes = [];
+    for (let i = 0; i < HOLES; i++) {
+      this.holes[i] = START_STONES;
     }
+    this.holes[P1_DEST] = 0;
+    this.holes[P2_DEST] = 0;
+    this.turn = P1;
+  }
 
-    move(hole) {
-        if (this.holeBelongsTo(hole, this.turn)) {
-            let stones = this.holes[hole];
-            let counter = hole;
-            this.holes[hole] = 0;
-            while (stones > 0) {
-                counter++;
-                counter = counter % HOLES;
-                this.holes[counter]++;
-                stones--;
-            }
-            if (!this.isDestHole(counter))
-                this.switchTurn();
-        } else throw new Error("That hole does not belong to the current player.");
-    }
+  move(hole) {
+    if (this.holeBelongsTo(hole, this.turn)) {
+      let hit = [];
+      let stones = this.holes[hole];
+      let counter = hole;
+      this.holes[hole] = 0;
+      while (stones > 0) {
+        counter++;
+        counter = counter % HOLES;
+        hit.push(counter);
+        this.holes[counter]++;
+        stones--;
+      }
+      if (!this.isDestHole(counter))
+        this.switchTurn();
+      return hit;
+    } else throw new Error("That hole does not belong to the current player.");
+  }
 
-    holeBelongsTo(hole, player) {
-        if (player === P1) {
-            return hole < P1_DEST && hole > 0;
-        } else if (player === P2) {
-            return hole < HOLES && hole > P1_DEST;
-        } else return false;
-    }
+  holeBelongsTo(hole, player) {
+    if (player === P1) {
+      return hole < P1_DEST && hole > 0;
+    } else if (player === P2) {
+      return hole < HOLES && hole > P1_DEST;
+    } else return false;
+  }
 
-    isDestHole(hole) {
-        return hole === P1_DEST || hole === P2_DEST;
-    }
+  isDestHole(hole) {
+    return hole === P1_DEST || hole === P2_DEST;
+  }
 
-    switchTurn() {
-        this.turn = this.turn === P1 ? P2 : P1;
-    }
+  switchTurn() {
+    this.turn = this.turn === P1 ? P2 : P1;
+  }
 }
--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const bodyParser = require('body-parser');
 
 const app = express();
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 
 app.use(express.static('public'));
 
@@ -11,17 +13,23 @@ let Mancala = require('./model/Mancala.js');
 let _game = new Mancala();
 
 app.get('/api/game', (req, res) => {
-    res.send(_game);
+  res.send(_game);
 });
 
 app.post('/api/game/move', (req, res) => {
-    try {
-        console.log(req.body.hole);
-        _game.move(req.body.hole);
-        res.send({success: true, game: _game});
-    } catch(error) {
-        res.send({success: false, error: error.message})
-    }
+  try {
+    let hit = _game.move(req.body.hole);
+    res.send({
+      success: true,
+      game: _game,
+      hit: hit,
+    });
+  } catch (error) {
+    res.send({
+      success: false,
+      error: error.message
+    })
+  }
 });
 
 app.listen(3000, () => console.log('Server listening on port 3000!'))
